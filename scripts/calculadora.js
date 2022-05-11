e1f5605f08791fa5299f99b40c3d329268f529de
Cláudia de Castro Gonçalves <dcastrg@gmail.com>
2022-05-11 17:48:22 -0300

    debug

---
+++ scripts/calculadora.js
@@ -0,0 +1,14 @@
+//teremos eventos na nossa calculadora//
+/**
+ * objetivo será configurar os elementos da minha calcurladora
+ */
+function configurar () {
+    console.log("configurar");
+}
+
+//#region Funcoes do painel
+function configurarAbrirHistorico() {
+    console.log("configurar");
+}
+
+//#endregion
